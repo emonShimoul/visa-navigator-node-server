26229bb7430094617efcad99d023e79500ea9575
emonShimoul <emonshimoul@gmail.com>
2025-03-05 14:00:43 +0600

    implement add a visa in the database

--- index.js
+++ index.js
@@ -27,10 +27,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const visaCollection = client.db("visaNavigatorDB").collection("visa");
+
+    app.post("/visas", async (req, res) => {
+      const newVisa = req.body;
+      console.log("data: ", newVisa);
+
+      const result = await visaCollection.insertOne(newVisa);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
