18630e2d04197599146c246c989544fa36a909cf
emonShimoul <emonshimoul@gmail.com>
2025-03-08 11:55:42 +0600

    visa delete functionality from database

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/visas/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // for visa application
     app.post("/visa-application", async (req, res) => {
       const newVisaApplication = req.body;
