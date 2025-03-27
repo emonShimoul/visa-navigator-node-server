ba93050bbe1a7b902e67bab483701deee242c3f8
emonShimoul <emonshimoul@gmail.com>
2025-03-27 19:00:10 +0600

    slight changes for deploy in live

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const visaCollection = client.db("visaNavigatorDB").collection("visa");
     const visaApplicationCollection = client
@@ -49,7 +48,6 @@ async function run() {
 
     app.get("/visas/:email", async (req, res) => {
       const email = req.params.email;
-      //   console.log(email);
       const query = { userEmail: email };
       const cursor = visaCollection.find(query);
       const result = await cursor.toArray();
@@ -58,7 +56,6 @@ async function run() {
 
     app.post("/visas", async (req, res) => {
       const newVisa = req.body;
-      console.log("data: ", newVisa);
       const result = await visaCollection.insertOne(newVisa);
       res.send(result);
     });
@@ -75,7 +72,6 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatedVisa = req.body;
-      console.log(updatedVisa);
 
       const visa = {
         $set: {
@@ -99,7 +95,6 @@ async function run() {
     // Visa application related APIs
     app.post("/visa-application", async (req, res) => {
       const newVisaApplication = req.body;
-      //   console.log("application: ", newVisaApplication);
       const result = await visaApplicationCollection.insertOne(
         newVisaApplication
       );
@@ -108,7 +103,6 @@ async function run() {
 
     app.get("/visa-application/:email", async (req, res) => {
       const email = req.params.email;
-      //   console.log(email);
       const query = { userEmail: email };
       const cursor = visaApplicationCollection.find(query);
       const result = await cursor.toArray();
@@ -131,7 +125,6 @@ async function run() {
 
     app.post("/users", async (req, res) => {
       const newUser = req.body;
-      //   console.log("Creating new user", newUser);
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
